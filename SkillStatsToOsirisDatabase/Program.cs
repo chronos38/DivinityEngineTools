603b894ac8489c2feb35abf69d56aae138756669
David Wolf <d.wolf@live.at>
2018-12-01 18:47:55 +0100

    Add project files.

---
+++ SkillStatsToOsirisDatabase/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace SkillStatsToOsirisDatabase
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
